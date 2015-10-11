c7a5c672ac67ca77b9db7f4abe09e766bc197f67
vishalshastri <vishaldshastri@gmail.com>
2015-10-11 22:12:48 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,10 +1,16 @@
-t1 <- read.table("household_power_consumption.txt", sep=";", stringsAsFactors=FALSE, na.strings=c("?"), header=TRUE)
+#read the file into data
+t1 <- read.table("../household_power_consumption.txt", sep=";", stringsAsFactors=FALSE, na.strings=c("?"), header=TRUE)
+
+#set the Date format
 t1$Date <- as.Date(t1$Date, "%d/%m/%Y")
 
+#subsetting the data that we want to focus on
 sub <- t1[t1$Date == "2007-02-01" | t1$Date == "2007-02-02",]
+
+#converting the date format
 sub$datetime <- strptime(paste(sub$Date, sub$Time), format="%Y-%m-%d %H:%M:%S")
 
-png(filename="plot4.png", width=480, height=480, units="px")
+#plotting using plot
 par(mfcol=c(2, 2))
 with(sub, plot(sub$datetime, sub$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
 
@@ -18,4 +24,6 @@ with(sub, plot(sub$datetime, Voltage, type="l", xlab="datetime"))
 
 with(sub, plot(sub$datetime, Global_reactive_power, type="l", xlab="datetime"))
 
+#save the file to plot4.png
+png(filename="plot4.png", width=480, height=480, units="px")
 dev.off()
